85c3d3ff989c3dc4eb58cbdb8978a183c1740524
kadenrose <156358267+kadenrose@users.noreply.github.com>
2025-11-01 05:52:21 -0700

    more practice

--- Main.java
+++ Main.java
@@ -11,7 +11,29 @@
 
 public class Main {
     public static void main(String[] args) {
+        // Create an array to hold 15 DVDs
         DVD[] dvdCollection = new DVD[15];
+
+        // Create DVD objects
+        DVD avengersDVD = new DVD("The Avengers", 2012, "Joss Whedon");
+        DVD incrediblesDVD = new DVD("The Incredibles", 2004, "Brad Bird");
+        DVD findingDoryDVD = new DVD("Finding Dory", 2016, "Andrew Stanton");
+        DVD lionKingDVD = new DVD("The Lion King", 2019, "Jon Favreau");
+        DVD starWarsDVD = new DVD("Star Wars", 1977, "George Lucas");
+
+        // Put DVDs into the array
+        dvdCollection[7] = avengersDVD;   
+        dvdCollection[3] = incrediblesDVD;  
+        dvdCollection[9] = findingDoryDVD;  
+        dvdCollection[2] = lionKingDVD;     
+        dvdCollection[0] = starWarsDVD;
+
+        // Print out the DVDs that were added
+        // for (int i = 0; i < dvdCollection.length; i++) {
+        //     if (dvdCollection[i] != null) {
+        //         System.out.println("Slot " + i + ": " + dvdCollection[i]);
+        //     }
+        // }
     }
 }
 
@@ -30,3 +52,4 @@ class DVD {
         return this.name + ", directed by " + this.director + ", released in " + this.releaseYear;
     }
 }
+
