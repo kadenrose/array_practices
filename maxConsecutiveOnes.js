60dbfd457c1a30d324f10ff37595824b90b185c9
kadenrose <156358267+kadenrose@users.noreply.github.com>
2025-11-04 14:38:31 -0700

    some crap

--- maxConsecutiveOnes.js
+++ maxConsecutiveOnes.js
@@ -8,14 +8,16 @@ let number = [1, 1, 0, 1, 1, 1];
 let c = 0;
 
 var findMaxConsecutiveOnes = function(nums) {
-    for(let num = 0; num < number.length; num++){
-        if (indexOf(number[num]) == 0){
+    for(let i = 0; i < number.length; i++){
+        if (i == 0){
             console.log("Beginning of array");
         }else{
-            if (number[num] == number[num - 1]){
+            if (number[i] == number[i - 1]){
             c++;
             }
         }
+        console.log(c);
     }
     console.log(c);
 };
+console.log(c);
